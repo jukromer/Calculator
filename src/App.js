267f6636b782e496998bef9004fd1ddb5fff1123
Julian Kromer <julian.kromer@outlook.com>
2021-08-11 13:49:45 +0200

    .

--- src/App.js
+++ src/App.js
@@ -24,6 +24,16 @@ class App extends React.Component {
     })
   }
 
+  handleAns = () => {
+    if (this.state.output === "") {
+      
+    }else{
+      this.setState({
+        input: this.state.output,
+        output: ""
+    })}
+  }
+
   handleEquals = value => () => {
     const { input } = this.state; // const input = this.state.input
     if (input.includes("-")) {
@@ -46,6 +56,7 @@ class App extends React.Component {
       });
     } else if (input.includes("*")){
       const ints = input.split("*").map(s => parseInt(s))
+      
       this.setState({
         output: ints[0]*ints[1],
         input: ""
@@ -63,22 +74,30 @@ class App extends React.Component {
       <div className="App">
         <div className="Calculator">
           <input onChange={this.handleInput('input')} value={this.state.input} type="text"></input>
-          <button onClick={this.handleButton("0")}>0</button>
-          <button onClick={this.handleButton("1")}>1</button>
-          <button onClick={this.handleButton("2")}>2</button>
-          <button onClick={this.handleButton("3")}>3</button>
-          <button onClick={this.handleButton("4")}>4</button>
-          <button onClick={this.handleButton("5")}>5</button>
-          <button onClick={this.handleButton("6")}>6</button>
+          <br/>
           <button onClick={this.handleButton("7")}>7</button>
           <button onClick={this.handleButton("8")}>8</button>
           <button onClick={this.handleButton("9")}>9</button>
-          <button onClick={this.handleButton("/")}>/</button>
-          <button onClick={this.handleButton("*")}>*</button>
-          <button onClick={this.handleButton("-")}>-</button>
-          <button onClick={this.handleButton("+")}>+</button>
-          <button onClick={this.handleEquals("huh")}>=</button>
           <button onClick={this.handleAC}>AC</button>
+          <br/>
+          <button onClick={this.handleButton("4")}>4</button>
+          <button onClick={this.handleButton("5")}>5</button>
+          <button onClick={this.handleButton("6")}>6</button>
+          <button onClick={this.handleButton("*")}>*</button>
+          <button onClick={this.handleButton("/")}>/</button>
+          <br/>
+          <button onClick={this.handleButton("1")}>1</button>
+          <button onClick={this.handleButton("2")}>2</button>
+          <button onClick={this.handleButton("3")}>3</button>
+          <button onClick={this.handleButton("+")}>+</button>
+          <button onClick={this.handleButton("-")}>-</button>
+          <br/>
+          <button onClick={this.handleButton("0")}>0</button>
+          <button onClick={this.handleButton(".")}>.</button>
+          <button onClick={this.handleEquals("huh")}>=</button>
+          <button onClick={this.handleAns}>Ans</button>
+
+          <br/>
           <input onChange={this.handleInput('output')} value={this.state.output} type="text"></input>
         </div>
       </div>
